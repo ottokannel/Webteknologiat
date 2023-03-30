f8db9ccaa4a380f456259b1f1cde06101d140245
Otto <otto.kannel@gmail.com>
2023-03-30 09:13:31 +0300

    Test commit

---
+++ Webteknologiat - Javascript/Harjoitus1/js/helloworld.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+console.log("Tämä kirjoitetaan selaimen konsoliin");
+}
+function sayHelloTo(name) {
+    alert("Hei " + name + "!");
+    console.log("sayHello-funktio suoritettu");
+    }
+    
