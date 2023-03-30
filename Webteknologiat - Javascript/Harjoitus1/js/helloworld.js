abede2329ef8e063a7ae666c9dfef725ed5ef23e
ottokannel <122344808+ottokannel@users.noreply.github.com>
2023-03-30 09:18:41 +0300

    Delete Webteknologiat - Javascript/Harjoitus1 directory

--- Webteknologiat - Javascript/Harjoitus1/js/helloworld.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-console.log("Tämä kirjoitetaan selaimen konsoliin");
-}
-function sayHelloTo(name) {
-    alert("Hei " + name + "!");
-    console.log("sayHello-funktio suoritettu");
-    }
-    
