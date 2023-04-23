495e3452bcad34dff90f66da35e573b97401c4fb
Otto <otto.kannel@gmail.com>
2023-04-24 00:42:30 +0300

    Commit

--- Webteknologiat - Javascript/Ajax/JS/Ajax.js
+++ Webteknologiat - Javascript/Ajax/JS/Ajax.js
@@ -1,3 +1,7 @@
+let searchCount = parseInt(localStorage.getItem('searchCount')) || 0;
+const counter = document.getElementById('counter');
+counter.textContent = searchCount;
+
 const loadingAnimation = document.querySelector('#loading-animation');
 const resultContainer = document.querySelector('#result-container');
 const searchButton = document.querySelector('#fetch-button');
@@ -5,6 +9,8 @@ const searchButton = document.querySelector('#fetch-button');
 searchButton.addEventListener('click', () => {
   // Näytä animaatio
   loadingAnimation.style.display = 'block';
-  // Piilota animaatio
-  loadingAnimation.style.display = 'none';
-});
+      // Piilota animaatio
+      loadingAnimation.style.display = 'none';
+    });
+
+counter.textContent = searchCount;
